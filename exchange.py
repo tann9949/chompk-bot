0c29318e874f548080699beebeda1741e56f9837
Natawat Kwanpum <natawat.kwanpum@agoda.com>
2021-10-25 10:34:49 +0700

    Add github action for BTC OKEX pairs.

---
+++ exchange.py
@@ -0,0 +1,4 @@
+from enum import Enum
+class Exchange(Enum):
+    BINANCE = 1
+    OKEX = 2
--- send_summary.py
+++ send_summary.py
@@ -1,9 +1,11 @@
 import os
 import logging
+import sys
 
 from dotenv import load_dotenv
 
 from bot import Bot
+from exchange import Exchange
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -19,9 +21,19 @@ def init_dotenv():
 def main():
     # load .env and unpack
     env = init_dotenv()
+    pair_arg = sys.argv[1]
+    
+    (pair, exchange) = getPairAndExchange(pair_arg)
+    logging.info(f"sending summary for {pair} pairs from {exchange}")
+    
     bot = Bot(token=env["token"])
-    bot.send_message_to_chat(env["chat_id"])  # to directly send to chat
+    bot.send_message_to_chat(env["chat_id"], pair, exchange)
 
+def getPairAndExchange(pair_arg: str):
+    if pair_arg.lower == "btc":
+        return ("btc", Exchange.OKEX)
+    else:
+        return ("usdt", Exchange.BINANCE)
 
 if __name__ == "__main__":
     main()
