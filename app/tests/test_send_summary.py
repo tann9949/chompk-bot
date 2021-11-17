fe37dc3a7f1f3c659c10cd5cfe736f5b4d8e6160
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2021-11-17 12:04:03 +0700

    fix pair dependency to be exchange

--- app/tests/test_send_summary.py
+++ app/tests/test_send_summary.py
@@ -1,15 +1,10 @@
 import pytest
 from app.enums.exchange import Exchange
 
-from send_summary import getPairAndExchange
 
-def test_getPairAndExchange_btc():
-    (pair, exchange) = getPairAndExchange("btc")
-    assert(pair == 'btc')
-    assert(exchange == Exchange.OKEX)
+def test_getPairAndExchange_okex():
+    pass
 
 
-def test_getPairAndExchange_usdt():
-    (pair, exchange) = getPairAndExchange("usdt")
-    assert(pair == 'usdt')
-    assert(exchange == Exchange.BINANCE)
+def test_getPairAndExchange_binance():
+    pass
