a7526f47e2f928d010c3958f6b073ac7338e9669
Kunanon BURATHEP <phankunanon@gmail.com>
2021-11-25 21:04:07 +0700

    add kucoin api for bot usdt and btc pair

--- app/api.py
+++ app/api.py
@@ -143,6 +143,7 @@ class BinanceAPI:
                 and ticker["symbol"].count("BTC") == 1
             )
         ]
+        
 class FtxAPI:
     base_url: str = "https://ftx.com/api"
     reso_mapping: Dict[str, int] = {
@@ -214,6 +215,67 @@ class FtxAPI:
                 and ticker["name"].count("BTC") == 1
                 and "DAI" not in ticker["name"]
                 and "-" not in ticker["name"]
+            )
+        ]
+
+
+class KucoinAPI:
+
+    base_url: str = "https://api.kucoin.com"
+
+    @staticmethod
+    def generate_candle_data(
+        symbol: str, 
+        interval: str = "1day") -> pd.DataFrame:
+        r = requests.get(f"{KucoinAPI.base_url}/api/v1/market/candles", {"symbol": symbol, "type": interval})
+        klines = json.loads(r.text)
+        candle_data = []
+        timestamp = []
+        for l in klines["data"]:
+            open_time = (datetime.utcfromtimestamp(int(l[0])).strftime('%Y-%m-%d %H:%M:%S'))
+            timestamp.append(open_time)
+            # end_time = datetime.utcfromtimestamp(l[6])
+            volume = l[5]
+            high, low, op, close = l[3], l[4], l[1], l[2]
+            candle_data.append([op, close, high, low, volume])
+        candle_data = pd.DataFrame(candle_data, columns=["open", "close", "high", "low", "volume"], index=timestamp)
+        return candle_data.astype(float)
+    
+    @staticmethod
+    def get_usdt_tickers() -> List[str]:
+        r = requests.get(f"{KucoinAPI.base_url}/api/v1/symbols")
+        tickers = json.loads(r.text)
+        return [
+            ticker["symbol"]
+            for ticker in tickers["data"]
+            if (
+                ticker["symbol"][:4] != "USDT"
+                and "USDT" in ticker["symbol"] 
+                and "UP" not in ticker["symbol"]
+                and "DOWN" not in ticker["symbol"]
+                and "BEAR" not in ticker["symbol"]
+                and "BULL" not in ticker["symbol"]
+                and ticker["symbol"].count("USD") == 1
+                and "DAI" not in ticker["symbol"]
+            )
+        ]
+
+    @staticmethod
+    def get_btc_tickers() -> List[str]:
+        r = requests.get(f"{KucoinAPI.base_url}/api/v1/symbols")
+        tickers = json.loads(r.text)
+        return [
+            ticker["symbol"]
+            for ticker in tickers["data"]
+            if (
+                ticker["symbol"][:3] != "BTC"
+                and "USD" not in ticker["symbol"] 
+                and "BTC" in ticker["symbol"]
+                and "DOWN" not in ticker["symbol"]
+                and "BEAR" not in ticker["symbol"]
+                and "BULL" not in ticker["symbol"]
+                and "DAI" not in ticker["symbol"]
+                and ticker["symbol"].count("BTC") == 1
             )
         ]
 
