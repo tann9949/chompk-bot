db23b36a32b0f66a82852d9a2e766c62504c66d2
Chompakorn CChaichot <tan_epacsp@hotmail.com>
2022-02-28 12:12:04 +0700

    fixed pytest not to send an actual message

--- app/tests/test_send_summary.py
+++ app/tests/test_send_summary.py
@@ -2,56 +2,31 @@ import os
 
 from dotenv import load_dotenv
 
-from app.bot import Bot
-
-
-def test_initenv():
-    global env
-    load_dotenv()
-
-    token = os.getenv("TOKEN", "TOKEN")
-    binance_chat_id = os.getenv("BINANCE_CHAT_ID", "BINANCE_CHAT_ID")
-    okex_chat_id = os.getenv("OKEX_CHAT_ID", "OKEX_CHAT_ID")
-    ftx_chat_id = os.getenv("FTX_CHAT_ID", "FTX_CHAT_ID")
-    kucoin_chat_id = os.getenv("KUCOIN_CHAT_ID", "KUCOIN_CHAT_ID")
-    bitkub_chat_id = os.getenv("BITKUB_CHAT_ID", "BITKUB_CHAT_ID")
-    env = {
-        "token": token,
-        "chat_id": {
-            "binance": binance_chat_id,
-            "okex": okex_chat_id,
-            "ftx": ftx_chat_id,
-            "kucoin": kucoin_chat_id,
-            "bitkub": bitkub_chat_id,
-        }
-    }
+from app.callback import get_cdc_template
+from app.enums.exchange import Exchange
+from app.enums.pairs import Pairs
 
 
 def test_okex():
-    exchange = "okex"
-    bot = Bot(token=env["token"])
-    bot.send_message_to_chat(env["chat_id"][exchange], exchange)
+    get_cdc_template(Pairs.USDT, Exchange.OKEX)
+    get_cdc_template(Pairs.BTC, Exchange.OKEX)
 
 
 def test_bitkub():
-    exchange = "bitkub"
-    bot = Bot(token=env["token"])
-    bot.send_message_to_chat(env["chat_id"][exchange], exchange)
+    get_cdc_template(Pairs.THB, Exchange.BITKUB)
 
 
 def test_binance():
-    exchange = "binance"
-    bot = Bot(token=env["token"])
-    bot.send_message_to_chat(env["chat_id"][exchange], exchange)
+    get_cdc_template(Pairs.USDT, Exchange.BINANCE)
+    get_cdc_template(Pairs.BTC, Exchange.BINANCE)
 
 
 def test_ftx():
-    exchange = "ftx"
-    bot = Bot(token=env["token"])
-    bot.send_message_to_chat(env["chat_id"][exchange], exchange)
+    get_cdc_template(Pairs.USDT, Exchange.FTX)
+    get_cdc_template(Pairs.PERP, Exchange.FTX)
+    get_cdc_template(Pairs.BTC, Exchange.FTX)
 
 
 def test_kucoin():
-    exchange = "kucoin"
-    bot = Bot(token=env["token"])
-    bot.send_message_to_chat(env["chat_id"][exchange], exchange)
+    get_cdc_template(Pairs.USDT, Exchange.KUCOIN)
+    get_cdc_template(Pairs.BTC, Exchange.KUCOIN)
