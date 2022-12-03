6fb8b42d068c7dadcc3bace18d9dd46c06296afe
Chompakorn CChaichot <chompakornc_pro@vistec.ac.th>
2022-12-03 21:16:58 +0700

    remove FTX from the list

--- send_summary.py
+++ send_summary.py
@@ -20,7 +20,6 @@ def init_dotenv():
     token = os.getenv("TOKEN", "TOKEN")
     binance_chat_id = os.getenv("BINANCE_CHAT_ID", "BINANCE_CHAT_ID")
     okex_chat_id = os.getenv("OKEX_CHAT_ID", "OKEX_CHAT_ID")
-    ftx_chat_id = os.getenv("FTX_CHAT_ID", "FTX_CHAT_ID")
     kucoin_chat_id = os.getenv("KUCOIN_CHAT_ID", "KUCOIN_CHAT_ID")
     bitkub_chat_id = os.getenv("BITKUB_CHAT_ID", "BITKUB_CHAT_ID")
     return {
@@ -28,7 +27,6 @@ def init_dotenv():
         "chat_id": {
             "binance": binance_chat_id,
             "okex": okex_chat_id,
-            "ftx": ftx_chat_id,
             "kucoin": kucoin_chat_id,
             "bitkub": bitkub_chat_id,
         }
